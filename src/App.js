bb299f70a9818d54ddb550266eac8ce12bea7df6
Brian Schmeling <brianschmeling2@gmail.com>
2023-02-01 14:05:14 -0500

    Begin work on layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Translate from "./Components/Translate/Translate";
 
 function App() {
   return (
     <div>
       <header></header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Translate />} />
+        </Routes>
+      </main>
     </div>
   );
 }
